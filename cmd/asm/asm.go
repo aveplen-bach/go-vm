8ab5ab9d3a382063adf7754f36d9bc2b1979c704
Alexey Plenkin <aveplen@yandex.ru>
2022-10-25 04:09:00 +0300

    add simple asm interface

--- cmd/asm/asm.go
+++ cmd/asm/asm.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"encoding/binary"
+	"fmt"
+	"os"
+
+	"github.com/aveplen/sm/internal"
+)
 
 func main() {
-	fmt.Println("i am compiler")
+	input := os.Args[1]
+	fin, err := os.Open(input)
+	defer fin.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	finr := bufio.NewReader(fin)
+	program := internal.Compile(*finr)
+	fmt.Println(len(program))
+
+	output := os.Args[2]
+	fout, err := os.Create(output)
+	defer fout.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	foutw := bufio.NewWriter(fout)
+	binary.Write(foutw, binary.LittleEndian, program)
+	foutw.Flush()
 }
