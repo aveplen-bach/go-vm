7478baa2783225981a80473ed4d39117d6a47e6f
Alexey Plenkin <aveplen@yandex.ru>
2022-10-25 17:26:07 +0300

    add vm cmd

--- cmd/vm/vm.go
+++ cmd/vm/vm.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"encoding/binary"
+	"os"
+	"strconv"
+
+	"github.com/aveplen/sm/internal"
+)
 
 func main() {
-	fmt.Println("i am vm")
+	input := os.Args[1]
+	fin, err := os.Open(input)
+	defer fin.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	stat, err := fin.Stat()
+	if err != nil {
+		panic(err)
+	}
+
+	program := make([]uint32, stat.Size()/4)
+	if err := binary.Read(fin, binary.LittleEndian, program); err != nil {
+		panic(err)
+	}
+
+	strmeminit := os.Args[2:]
+	meminit := make([]uint32, 0, 10)
+	for _, v := range strmeminit {
+		uintmem, err := strconv.ParseUint(v, 10, 32)
+		if err != nil {
+			panic(err)
+		}
+
+		meminit = append(meminit, uint32(uintmem))
+	}
+
+	cpu := internal.WithMemProg(meminit, program)
+	cpu.Run()
 }
